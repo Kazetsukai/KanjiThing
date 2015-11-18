edfeae4345a6501e9468fd0c18f9fe17ae727bac
Daniel Tebbutt <moeruze.burning@gmail.com>
2015-11-19 05:52:05 +1300

    Added kanjiwriter

---
+++ KanjiWriter/KanjiWriter/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace KanjiWriter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(64, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 218);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(343, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(343, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Unicode Character";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 366);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Kanji/Kana Writer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ KanjiWriter/KanjiWriter/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using System.Xml.Serialization;
+
+namespace KanjiWriter
+{
+    public partial class Form1 : Form
+    {
+        private bool _started = false;
+        private bool _drawing = false;
+        private List<List<TimePoint>> _segments;
+        private List<TimePoint> _points; 
+        private DateTime _start;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!_started)
+            {
+                _segments = new List<List<TimePoint>>();
+                _start = DateTime.Now;
+                _started = true;
+            }
+            if (!_drawing)
+            {
+                _points = new List<TimePoint>();
+                _segments.Add(_points);
+                _drawing = true;
+            }
+
+            AddPoint(e.X, e.Y);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _drawing = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_drawing)
+                AddPoint(e.X, e.Y);
+        }
+
+        private void AddPoint(int x, int y)
+        {
+            _points.Add(new TimePoint
+            {
+                X = x, 
+                Y = y, 
+                TimeFromStart = (int)(DateTime.Now - _start).TotalMilliseconds
+            });
+
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawLines(e.Graphics);
+        }
+
+        private void DrawLines(Graphics graphics)
+        {
+            if (_segments == null)
+                return;
+
+            foreach (var segment in _segments)
+            {
+                if (segment.Count > 1)
+                    graphics.DrawLines(Pens.Black, segment.Select(p => new Point(p.X, p.Y)).ToArray());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Save kana/kanji //
+            /////////////////////
+            
+            // Get the unicode point
+            if (textBox1.Text.Length != 1)
+                MessageBox.Show("Please enter exactly one unicode character in textbox");
+
+            // This might not strictly be the most correctest unicoding...
+            var codePoint = string.Format("{0:X5}", char.ConvertToUtf32(textBox1.Text, 0));
+            this.Text = "U+" + codePoint;
+
+            var dir = @"kanji\" + codePoint;
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            // Get the highest existing number, and go one up.
+            int maxNum = -1;
+            var listing = Directory.EnumerateFiles(dir).Select(f => new FileInfo(f).Name);
+            foreach (var file in listing)
+            {
+                int num = 0;
+                if (int.TryParse(file.Split('.').DefaultIfEmpty("").First(), out num) && num > maxNum)
+                    maxNum = num;
+            }
+            maxNum++;
+
+            var filenameData = dir + "\\" + maxNum + ".xml";
+            var filenamePng = dir + "\\" + maxNum + ".png";
+
+            using (var stream = File.OpenWrite(filenameData))
+            {
+                var xmlSerializer = new XmlSerializer(typeof (List<List<TimePoint>>));
+                xmlSerializer.Serialize(stream, _segments);
+            }
+
+            Bitmap b = new Bitmap(109, 109);
+            var graphics = Graphics.FromImage(b);
+            graphics.ScaleTransform(0.5f, 0.5f);
+            DrawLines(graphics);
+            b.Save(filenamePng);
+
+            _segments = null;
+            _started = false;
+            pictureBox1.Invalidate();
+        }
+    }
+
+    public struct TimePoint
+    {
+        public int Y { get; set; }
+        public int X { get; set; }
+        public int TimeFromStart { get; set; }
+    }
+}
