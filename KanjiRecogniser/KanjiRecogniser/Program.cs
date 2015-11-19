cd81af12ac3c2dbfcc07f238d67b68de31571596
Daniel Tebbutt <moeruze.burning@gmail.com>
2015-11-20 06:00:47 +1300

    Adding kanji recogniser project with samples

---
+++ KanjiRecogniser/KanjiRecogniser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanjiRecogniser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
